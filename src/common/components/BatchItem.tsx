ab8fa820f55649b96394e65d1acd27664bc86635
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-19 09:34:57 +0530

    refactor: design changes

--- src/common/components/BatchItem.tsx
+++ src/common/components/BatchItem.tsx
@@ -26,55 +26,39 @@ export default function BatchItem({
 
   // Convert batchName using mapping or use the original
   const fullBatchName = batchNameMap[batchName] || batchName;
-console.log(batchName)
+
   return (
-    <div className="flex flex-col bg-white p-4 rounded-lg border border-azure-100 w-[18rem] gap-5 h-[245px]">
-      <div className="flex flex-row items-center gap-4">
-        <span className="text-2xl font-semibold text-azure-600 uppercase">
-          {batchCode}
-        </span>
-        <div className="w-[1px] h-[2rem] bg-gray-400"></div>
-        <span className="text-base font-normal text-gray-700">
-          {fullBatchName}
-        </span>
-      </div>
-      <div className="flex flex-row items-center gap-2">
-        <span className="text-gray-800">Total students:</span>
-        <span className="text-xl font-semibold text-azure-600">
-          {totalStudents}
-        </span>
-      </div>
-      <div className="h-[1px] w-full bg-gray-300"></div>
-      <div className="w-full flex flex-col space-y-2">
-        <button
-          onClick={() => {
-            if (location === "/dashboard/attendance") {
-              router.push(`/dashboard/attendance/${batchCode}/mark-attendance/today`);
-            } else {
-              router.push(`/dashboard/batches/stats/${batchCode}`);
-            }
-          }}
-          className="bg-azure-600 w-full text-white font-medium p-2 outline-none border-none rounded-[7px]"
-        >
-          {location === "/dashboard/attendance"
-            ? " Mark Todays Attendance"
-            : " View Attendance Stats"}
-        </button>
-        <button
-          onClick={() => {
-            if (location === "/dashboard/attendance") {
-              router.push(`/dashboard/attendance/${batchCode}/mark-attendance/custom-date`);
-            } else {
-              router.push(`/dashboard/batches/${batchCode}/date-chart`);
-            }
-          }}
-          className="bg-azure-50 w-full text-azure-600 font-medium p-2 outline-none border-none rounded-[7px]"
-        >
-          {location === "/dashboard/attendance"
-            ? "Custom date"
-            : "View Date Chart"}
-        </button>
-      </div>
+    <div className="flex flex-col bg-white p-4 rounded-lg border border-azure-100 gap-5 h-[245px]">
+    <div className="flex flex-row items-center gap-4">
+      <span className="text-2xl font-semibold text-azure-600 uppercase">
+        {batchCode}
+      </span>
+      <div className="w-[1px] h-[2rem] bg-gray-400"></div>
+      <span className="text-base font-normal text-gray-700">
+        {fullBatchName}
+      </span>
     </div>
+    <div className="flex flex-row items-center gap-2">
+      <span className="text-gray-800">Total students:</span>
+      <span className="text-xl font-semibold text-azure-600">
+        {totalStudents}
+      </span>
+    </div>
+    <div className="h-[1px] w-full bg-gray-300"></div>
+    <div className="w-full flex flex-col space-y-2">
+      <button
+        onClick={() => router.push(`/dashboard/attendance/${batchCode}/mark-attendance/today`)}
+        className="bg-azure-600 w-full text-white font-medium p-2 rounded-md"
+      >
+        Mark Today's Attendance
+      </button>
+      <button
+        onClick={() => router.push(`/dashboard/attendance/${batchCode}/mark-attendance/custom-date`)}
+        className="bg-azure-50 w-full text-azure-600 font-medium p-2 rounded-md"
+      >
+        Custom Date
+      </button>
+    </div>
+  </div>
   );
 }
